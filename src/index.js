81c25313177bf2011c28da909bfa5a2362864006
Oscar G. Rodrigues <g.rodrigues.oscar@gmail.com>
2018-11-13 13:31:41 +0100

    correction redux pour mozilla

--- src/index.js
+++ src/index.js
@@ -8,9 +8,7 @@ import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import CounterReducer from './CounterReducer';
 
-const store = createStore(CounterReducer,
-	window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-);
+const store = createStore(CounterReducer);
 
 ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 
